6f5b89ba66e7a121922116009cc10b5152474495
Song Li <da_tou_li@yahoo.com>
2020-03-29 16:42:18 -0400

    Added the jira handler

---
+++ slack-support-bot-jira-handler/handlers/jira-handler.js
@@ -0,0 +1,47 @@
+const JIRA_HOST = process.env.JIRA_HOST;
+const JIRA_AUTH_EMAIL = process.env.JIRA_AUTH_EMAIL;
+const JIRA_AUTH_TOKEN = process.env.JIRA_AUTH_TOKEN;
+
+const JiraClient = require("jira-connector");
+
+const create = async (request) => {
+  const jira = new JiraClient({
+    host: JIRA_HOST,
+    basic_auth: {
+      email: JIRA_AUTH_EMAIL,
+      api_token: JIRA_AUTH_TOKEN
+    }
+  });
+
+  let data = {
+
+    fields: {
+      project: { key: 'SLE' },
+      summary: 'This is the summary No.1',
+      description: 'Creating of an issue using project keys and issue type names using the REST API',
+      reporter: {
+        accountId: '5e7e9cec1e65980c42c12b5d'
+      },
+      assignee: {
+        accountId: '5e7e9cec1e65980c42c12b5d'
+      },
+      duedate: '2020-04-12',
+      labels: [ 'da_tou_li' ],
+      priority: {
+        name: 'Medium'
+      },
+      issuetype: { name: 'Bug' },
+    }
+  };
+
+  jira.issue.createIssue(
+    data, (error, issue) => {
+      console.log(error);
+      console.log(issue);
+    }
+  );
+
+};
+
+exports.create = create;
+
